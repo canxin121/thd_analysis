f1ff534f6059a7a71b5cbf255996f4842f1be54a
canxin <q1969730106@gmail.com>
2025-04-11 13:44:24 +0800

    [Update] add github action files

--- control_flutter/android/app/build.gradle.kts
+++ control_flutter/android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.control_flutter"
+    namespace = "com.canxin.thd.analysis"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.control_flutter"
+        applicationId = "com.canxin.thd.analysis"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
---
+++ control_flutter/android/app/src/main/kotlin/com/canxin/thd/analysis/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.canxin.thd.analysis
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
